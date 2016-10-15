c58b95e0492aae24bcb882c5a47d50964f48a9d1
KJ Salchow <kjsalchow@me.com>
2016-10-15 11:24:21 -0500

    adding modified best.r#

--- best.R
+++ best.R
@@ -1,6 +1,6 @@
 ## Write a function called best that take two arguments: the 2-character 
 ## abbreviated name of a state and an outcome name. The function reads the 
-## outcome-of-care-measures.csv le and returns a character vector with the 
+## outcome-of-care-measures.csv file and returns a character vector with the 
 ## name of the hospital that has the best (i.e. lowest) 30-day mortality for 
 ## the specified outcome in that state. The hospital name is the name provided 
 ## in the Hospital.Name variable. The outcomes can be one of \heart attack", 
@@ -31,11 +31,34 @@
 
 best <- function(state, outcome) {
     ## Read outcome data
+    outcomeMeasures<- read.csv("outcome-of-care-measures.csv", 
+                               na.strings = "Not Available", stringsAsFactors = FALSE)
+    outcomes <- list(heart.attack="heart attack", heart.failure="heart failure",
+                     pneumonia="pneumonia")
     
     ## Check that state and outcome are valid
+    if (!(state %in% outcomeMeasures$State)) {
+        stop("invalid state")
+    }
+    
+    if (!(outcome %in% outcomes)){
+        stop("invalid outcome")
+    }
     
     ## Return hospital name in the state with the lowest 30-day death rate
-    
+    if (outcome == outcomes$heart.attack){
+        mysort <- outcomeMeasures[!is.na(outcomeMeasures[,11]) & outcomeMeasures[,7] == state,c(2,11)]
+    }
+    else if (outcome == outcomes$heart.failure){
+        mysort <- outcomeMeasures[!is.na(outcomeMeasures[,17]) & outcomeMeasures[,7] == state,c(2,17)]
+    }
+    else {
+        mysort <- outcomeMeasures[!is.na(outcomeMeasures[,23]) & outcomeMeasures[,7] == state,c(2,23)]
+    }
+    ## Sort by mortality rate (lowest) and state (alphabetical)
+    mysort <- mysort[order(mysort[,2], mysort[,1]),]
+    ## Return the first row of the sorted list
+    mysort[1,1]
 }
 
 
